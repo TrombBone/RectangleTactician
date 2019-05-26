12e052cf95f9877e0445de7633fc770d64baffbb
TrombBone <sameperson3@mail.ru>
2019-05-26 23:24:16 +0800

    добавил запоминание координат всех точек всех прямоугольников за игру

--- app/src/main/java/com/konstbone/finishitschoolproject_rectangletactician/MainGameActivity.java
+++ app/src/main/java/com/konstbone/finishitschoolproject_rectangletactician/MainGameActivity.java
@@ -1,5 +1,6 @@
 package com.konstbone.finishitschoolproject_rectangletactician;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
@@ -7,14 +8,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Objects;
+
+import static com.konstbone.finishitschoolproject_rectangletactician.TestSurfaceView.oneRectCoord;
 
 public class MainGameActivity extends AppCompatActivity {
 
     public static int rectSide1;
     public static int rectSide2;
-    public TextView exceptionTV;
+
+    public static int NumOfSquareInWidth = 20;
+    public static int NamOfSquareInHeight = 28;
+
+    public static ArrayList<ArrayList<ArrayList<Integer>>> rectCoord = new ArrayList<>();
+
+    public static TextView exceptionTV;
     public static boolean exceptionFlag = true;
+
     TestSurfaceView surfaceView;
 
     @Override
@@ -22,11 +33,16 @@ public class MainGameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_game);
         Objects.requireNonNull(getSupportActionBar()).hide();
-        surfaceView = (TestSurfaceView) findViewById(R.id.testSurfaceView_ClassElement);
+
+        surfaceView = findViewById(R.id.testSurfaceView_ClassElement);
         surfaceView.mainGameActivity = this;
         exceptionTV = findViewById(R.id.exceptionTV);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         //
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        surfaceView.getLayoutParams().height = NamOfSquareInHeight *surfaceView.getLayoutParams().width/20;
+
+        exceptionTV.setTextSize(exceptionTV.getLayoutParams().height);
 
         String rectSide1String = getIntent().getStringExtra("side1");
         String rectSide2String = getIntent().getStringExtra("side2");
@@ -35,21 +51,22 @@ public class MainGameActivity extends AppCompatActivity {
 
         exceptionTV_good_text_func();
     }
-    /*
-    Runnable exceptionTV_good_text_func = new Runnable() {
-        @Override
-        public void run() { }
-    };
-    */
-    public void exceptionTV_good_text_func() {
+
+    public static void exceptionTV_good_text_func() {
         exceptionFlag = true;
         exceptionTV.setText(R.string.exceptionTV_good_text);
         exceptionTV.setBackgroundColor(Color.GREEN);
     }
 
-    public void exceptionTV_bad_OutOfBoundsException_text_func() {
+    public static void exceptionTV_bad_OutOfBoundsException_text_func() {
         exceptionFlag = false;
         exceptionTV.setText(R.string.exceptionTV_bad_OutOfBoundsException_text);
+        exceptionTV.setBackgroundColor(Color.RED);
+    }
+
+    public static void exceptionTV_bad_LocationException_text_func() {
+        exceptionFlag = false;
+        exceptionTV.setText(R.string.exceptionTV_bad_LocationException_text);
         exceptionTV.setBackgroundColor(Color.RED);
     }
 
@@ -61,10 +78,13 @@ public class MainGameActivity extends AppCompatActivity {
                 rectSide2 = help;
                 break;
             case R.id.endPlayerProgressButton:
+                if (exceptionFlag) {
+                    rectCoord.add(oneRectCoord);
+                    Intent FromMainGameToPlayer2StartIntent = new Intent();
+                    startActivity(FromMainGameToPlayer2StartIntent);
+                }
                 //
                 break;
         }
     }
-
-
 }
--- app/src/main/java/com/konstbone/finishitschoolproject_rectangletactician/TestSurfaceView.java
+++ app/src/main/java/com/konstbone/finishitschoolproject_rectangletactician/TestSurfaceView.java
@@ -10,6 +10,18 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
+import static com.konstbone.finishitschoolproject_rectangletactician.MainGameActivity.NamOfSquareInHeight;
+import static com.konstbone.finishitschoolproject_rectangletactician.MainGameActivity.NumOfSquareInWidth;
+import static com.konstbone.finishitschoolproject_rectangletactician.MainGameActivity.exceptionFlag;
+import static com.konstbone.finishitschoolproject_rectangletactician.MainGameActivity.exceptionTV_bad_LocationException_text_func;
+import static com.konstbone.finishitschoolproject_rectangletactician.MainGameActivity.exceptionTV_bad_OutOfBoundsException_text_func;
+import static com.konstbone.finishitschoolproject_rectangletactician.MainGameActivity.exceptionTV_good_text_func;
+import static com.konstbone.finishitschoolproject_rectangletactician.MainGameActivity.rectCoord;
+import static com.konstbone.finishitschoolproject_rectangletactician.MainGameActivity.rectSide1;
+import static com.konstbone.finishitschoolproject_rectangletactician.MainGameActivity.rectSide2;
+
 
 public class TestSurfaceView extends SurfaceView implements SurfaceHolder.Callback{
 
@@ -22,16 +34,90 @@ public class TestSurfaceView extends SurfaceView implements SurfaceHolder.Callba
     }
 
     float touchX, touchY;
+    int partCanvasWidth;
+    float width, height;
     boolean v = false;
+    public static ArrayList<ArrayList<Integer>> oneRectCoord;
+    ArrayList<Integer> onePointCoord;
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
         touchX = event.getX();
         touchY = event.getY();
-        v = true;
-        exceptionTV = mainGameActivity.exceptionTV;
-        //всю проверку лучше сделать здесь!
+        exceptionTV = MainGameActivity.exceptionTV;
+
+        for (int x = 0; x < NumOfSquareInWidth*partCanvasWidth; x=x+partCanvasWidth) {
+            for (int y = 0; y < NamOfSquareInHeight*partCanvasWidth; y=y+partCanvasWidth) {
+                if ((touchX >= x && touchX <= x + partCanvasWidth)) {
+                    if (touchY >= y && touchY <= y + partCanvasWidth) {
+                        if ((x - partCanvasWidth*(rectSide1-1) >= 0 && y - partCanvasWidth*(rectSide2-1) >= 0)) {
+                            exceptionTV_good_text_func();
+                            v = true;
+                            oneRectCoord = new ArrayList<>();
+                            onePointCoord = new ArrayList<>();
+                            onePointCoord.add(x - partCanvasWidth*(rectSide1-1));
+                            onePointCoord.add(y - partCanvasWidth*(rectSide2-1));
+                            oneRectCoord.add(onePointCoord);
+                            onePointCoord = new ArrayList<>();
+                            onePointCoord.add(x + partCanvasWidth);
+                            onePointCoord.add(y - partCanvasWidth*(rectSide2-1));
+                            oneRectCoord.add(onePointCoord);
+                            onePointCoord = new ArrayList<>();
+                            onePointCoord.add(x - partCanvasWidth*(rectSide1-1));
+                            onePointCoord.add(y + partCanvasWidth);
+                            oneRectCoord.add(onePointCoord);
+                            onePointCoord = new ArrayList<>();
+                            onePointCoord.add(x + partCanvasWidth);
+                            onePointCoord.add(y + partCanvasWidth);
+                            oneRectCoord.add(onePointCoord);
+                        } else {
+                            exceptionTV_bad_OutOfBoundsException_text_func();
+                            v = false;
+                        }
+                        if (rectCoord.size() > 0) {
+                            check: for (int numRect = 0; numRect < rectCoord.size(); numRect++) {
+                                for (int numPointFirstRect = 0; numPointFirstRect < 4; numPointFirstRect++) {
+                                    for (int numPointSecondRect = 0; numPointSecondRect < 4; numPointSecondRect++) {
+                                        if (rectCoord.get(numRect).get(numPointFirstRect).get(0) == oneRectCoord.get(numPointSecondRect).get(0)) {
+                                            if (numPointFirstRect <= 1 && numPointSecondRect <= 1) {
+                                                if ((rectCoord.get(numRect).get(numPointFirstRect).get(1) >= oneRectCoord.get(numPointSecondRect).get(1) && rectCoord.get(numRect).get(numPointFirstRect+2).get(1) <= oneRectCoord.get(numPointSecondRect+2).get(1)) || (rectCoord.get(numRect).get(numPointFirstRect).get(1) <= oneRectCoord.get(numPointSecondRect).get(1) && rectCoord.get(numRect).get(numPointFirstRect+2).get(1) >= oneRectCoord.get(numPointSecondRect+2).get(1)) ) {
+                                                    exceptionTV_good_text_func();
+                                                    v = true;
+                                                    break check;
+                                                } else {
+                                                    exceptionTV_bad_LocationException_text_func();
+                                                    v = false;
+                                                }
+                                            } else {
+                                                // проверить, есть ли прямоугольники рядом
+                                            }
+                                        } else if (rectCoord.get(numRect).get(numPointFirstRect).get(1) == oneRectCoord.get(numPointSecondRect).get(1)) {
+                                            if (numPointFirstRect % 2 == 0 && numPointSecondRect % 2 == 0) {
+                                                if ((rectCoord.get(numRect).get(numPointFirstRect).get(0) >= oneRectCoord.get(numPointSecondRect).get(0) && rectCoord.get(numRect).get(numPointFirstRect+1).get(0) <= oneRectCoord.get(numPointSecondRect+1).get(0)) || (rectCoord.get(numRect).get(numPointFirstRect).get(0) <= oneRectCoord.get(numPointSecondRect).get(0) && rectCoord.get(numRect).get(numPointFirstRect+1).get(0) >= oneRectCoord.get(numPointSecondRect+1).get(0)) ) {
+                                                    exceptionTV_good_text_func();
+                                                    v = true;
+                                                    break check;
+                                                } else {
+                                                    exceptionTV_bad_LocationException_text_func();
+                                                    v = false;
+                                                }
+                                            } else {
+                                                // проверить, есть ли прямоугольники рядом
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        } else {
+                            //проверять грани старта
+                        }
+                        //
+                    }
+                }
+            }
+        }
+        //всю проверку - здесь!
         return true;
     }
 
@@ -57,48 +143,57 @@ public class TestSurfaceView extends SurfaceView implements SurfaceHolder.Callba
                 if (canvas != null) {
                     canvas.drawColor(Color.WHITE);
 
-                    float width = canvas.getWidth();
-                    float height = canvas.getHeight();// сетка
+                    width = canvas.getWidth();
+                    height = canvas.getHeight();
 
-                    int a = (int) (width/20);
+                    partCanvasWidth = (int) (width/20);
 
-                    drawPlayerOneRectangle(a, canvas);
-
-                    for (int x = 0; x < 20*a; x=x+a) {
-                        for (int y = 0; y < 28 * a; y = y + a) {
-                            canvas.drawRect(x, y, x + a, y + a, p);
+                    for (int x = 0; x < 20*partCanvasWidth; x=x+partCanvasWidth) {
+                        for (int y = 0; y < 28 * partCanvasWidth; y = y + partCanvasWidth) {
+                            canvas.drawRect(x, y, x + partCanvasWidth, y + partCanvasWidth, p);// сетка
                         }
                     }
+
+                    drawStartLines(canvas);
+
+                    drawPlayerOneRectangle(partCanvasWidth, canvas);
+
                     surfaceHolder.unlockCanvasAndPost(canvas);
                 }
             }
         }
 
+        public void drawStartLines(Canvas canvas) {
+            p.setColor(Color.RED);
+            canvas.drawLine(0, 0, 0, partCanvasWidth, p);
+            canvas.drawLine(1, 0, 1, partCanvasWidth, p);
+            canvas.drawLine(2, 0, 2, partCanvasWidth, p);
+            canvas.drawLine(3, 0, 3, partCanvasWidth, p);
+            canvas.drawLine(0, 0, partCanvasWidth, 0, p);
+            canvas.drawLine(0, 1, partCanvasWidth, 1, p);
+            canvas.drawLine(0, 2, partCanvasWidth, 2, p);
+            canvas.drawLine(0, 3, partCanvasWidth, 3, p);
+            p.setColor(Color.BLUE);
+            canvas.drawLine(NumOfSquareInWidth*partCanvasWidth, NamOfSquareInHeight*partCanvasWidth, NumOfSquareInWidth*partCanvasWidth - partCanvasWidth, NamOfSquareInHeight*partCanvasWidth, p);
+            canvas.drawLine(NumOfSquareInWidth*partCanvasWidth, NamOfSquareInHeight*partCanvasWidth-1, NumOfSquareInWidth*partCanvasWidth - partCanvasWidth, NamOfSquareInHeight*partCanvasWidth-1, p);
+            canvas.drawLine(NumOfSquareInWidth*partCanvasWidth, NamOfSquareInHeight*partCanvasWidth-2, NumOfSquareInWidth*partCanvasWidth - partCanvasWidth, NamOfSquareInHeight*partCanvasWidth-2, p);
+            canvas.drawLine(NumOfSquareInWidth*partCanvasWidth, NamOfSquareInHeight*partCanvasWidth-3, NumOfSquareInWidth*partCanvasWidth - partCanvasWidth, NamOfSquareInHeight*partCanvasWidth-3, p);
+            canvas.drawLine(NumOfSquareInWidth*partCanvasWidth, NamOfSquareInHeight*partCanvasWidth, NumOfSquareInWidth*partCanvasWidth, NamOfSquareInHeight*partCanvasWidth - partCanvasWidth, p);
+            canvas.drawLine(NumOfSquareInWidth*partCanvasWidth-1, NamOfSquareInHeight*partCanvasWidth, NumOfSquareInWidth*partCanvasWidth-1, NamOfSquareInHeight*partCanvasWidth - partCanvasWidth, p);
+            canvas.drawLine(NumOfSquareInWidth*partCanvasWidth-2, NamOfSquareInHeight*partCanvasWidth, NumOfSquareInWidth*partCanvasWidth-2, NamOfSquareInHeight*partCanvasWidth - partCanvasWidth, p);
+            canvas.drawLine(NumOfSquareInWidth*partCanvasWidth-3, NamOfSquareInHeight*partCanvasWidth, NumOfSquareInWidth*partCanvasWidth-3, NamOfSquareInHeight*partCanvasWidth - partCanvasWidth, p);
+            p.setColor(Color.BLACK);
+        }
+
         public void drawPlayerOneRectangle(int partCanvasWidth, Canvas canvas) {
             if (v) {
-                for (int x = 0; x < 20*partCanvasWidth; x=x+partCanvasWidth) {
-                    for (int y = 0; y < 28*partCanvasWidth; y=y+partCanvasWidth) {
+                for (int x = 0; x < NumOfSquareInWidth*partCanvasWidth; x=x+partCanvasWidth) {
+                    for (int y = 0; y < NamOfSquareInHeight*partCanvasWidth; y=y+partCanvasWidth) {
                         if ((touchX >= x && touchX <= x + partCanvasWidth)) {
                             if (touchY >= y && touchY <= y + partCanvasWidth) {
                                 p.setColor(Color.BLUE);
                                 p.setStyle(Paint.Style.FILL);
-                                /*
-                                ExceptionRectangle exceptionRectangle = new ExceptionRectangle(mainGameActivity, partCanvasWidth);
-                                if ((x - partCanvasWidth*(rectSide1-1) >= 0 && y - partCanvasWidth*(rectSide2-1) >= 0)) {
-                                    canvas.drawRect(x - partCanvasWidth*(rectSide1 - 1), y - partCanvasWidth*(rectSide2 - 1), x + partCanvasWidth, y + partCanvasWidth, p);
-                                    //писать в TextView о том, что всё хорошо и красить его в зелёный
-
-                                    exceptionFlag = true;
-
-                                } else {
-                                    exceptionFlag = false;
-                                }
-                                exceptionRectangle.start();
-                                try {
-                                    exceptionRectangle.join();
-                                } catch (InterruptedException e) { }
-                                */
-                                //писать в TextView об ошибке и красить его в красный
+                                canvas.drawRect(x - partCanvasWidth*(rectSide1 - 1), y - partCanvasWidth*(rectSide2 - 1), x + partCanvasWidth, y + partCanvasWidth, p);
                                 //
                                 //
                                 p.setColor(Color.BLACK);
